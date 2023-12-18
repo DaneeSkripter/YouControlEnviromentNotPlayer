711171f1da5ea5ef77e4c44f25a7a02bc6e2469b
daneeskripter <imdanee@seznam.cz>
2023-12-18 14:07:58 +0100

    mouseLook.cs works only if menu isn't on screen

--- Assets/Scripts/mouseLook.cs
+++ Assets/Scripts/mouseLook.cs
@@ -8,21 +8,18 @@ public class mouseLook : MonoBehaviour
     public float sensitivity = 300f;
     private float xRotation = 0f;
     public Transform playerBody;
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-
+    
     private void FixedUpdate()
     {
-        float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        xRotation -= y;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * x);
-        
+            xRotation -= y;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * x);
+        }
     }
 }
