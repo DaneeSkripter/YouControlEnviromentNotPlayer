27acef5e654e6fe0b045a30a9ce61f33098aa46b
daneeskripter <imdanee@seznam.cz>
2023-12-18 14:09:19 +0100

    HideMenu script added

---
+++ Assets/Scripts/hideMenu.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    // Start is called before the first frame update
+    public void checkIfMenuIsHidden()
+    {
+        if (menu.active)
+        {
+            HideMenu();
+        }
+        else
+        {
+            ShowMenu();
+        }
+    }
+
+    void HideMenu()
+    {
+        menu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void ShowMenu()
+    {
+        menu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    { 
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            ShowMenu();
+        }
+    }
+}
