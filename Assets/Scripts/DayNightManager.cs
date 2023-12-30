148e03386e6c2baad2d8a0b60b2718635286a6e9
daneeskripter <imdanee@seznam.cz>
2023-12-30 16:49:56 +0100

    daynight cycle added + time speed

---
+++ Assets/Scripts/DayNightManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightManager : MonoBehaviour
+{
+    public float dayLength = 600f;
+    public Light sun;
+    public float currentTime = 0f;
+    
+    void Update()
+    {
+        updateTime();
+        updateSun();
+    }
+
+    private int timeSpeed = 0;
+
+    void updateTime()
+    {
+        currentTime += Time.deltaTime * timeSpeed;
+        if (currentTime >= dayLength)
+        {
+            currentTime = 0f;
+        }
+    }
+
+    void updateSun()
+    {
+        float normalizedTime = currentTime / dayLength;
+
+        float angle = Mathf.Lerp(-90f, 270f, normalizedTime);
+        sun.transform.eulerAngles = new Vector3(angle, 0f, 0f);
+    }
+
+    public void setTimeSpeed(int speed)
+    {
+        timeSpeed = speed;
+    }
+}
